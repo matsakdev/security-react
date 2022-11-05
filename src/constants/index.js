f5fcdc324a996c0030c141ec049c07fcbb28f247
Stanislav Matsak <stanislav.matsak@nure.ua>
2022-11-05 03:02:38 +0200

    v1

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,9 @@
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'https://portalfood.azurewebsites.net';
 export const ACCESS_TOKEN = 'accessToken';
 
-export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
+export const WEB_BASE_URL = "https://gentle-tree-0064d1803.2.azurestaticapps.net"
+
+export const OAUTH2_REDIRECT_URI = WEB_BASE_URL + '/oauth2/redirect'
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const FACEBOOK_AUTH_URL = API_BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
